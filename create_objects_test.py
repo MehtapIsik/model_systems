85e135616b13010b0cf964acbe1b44f22f91da8b
Mehtap Isik <mehtap.isik@choderalab.org>
2014-12-15 17:12:02 -0500

    database: pdb to uniprot entery relation

--- create_objects_test.py
+++ create_objects_test.py
@@ -6,12 +6,17 @@ up_entry1 = db_models.Uniprot(acc="P00519")
 db.session.add(up_entry1)
 # up_entry2 = UniProt("acc 2" pdb_entry_2)
 
-pdb_entry_1 = db_models.PDB(pdb_id="PDB1", in_bindingdb_validation_set=False, uniprot_id=up_entry1.id)
-db.session.add(pdb_entry_1)
+                                                    #Relationship points to the uniprot object this way
+pdb_entry_1 = db_models.PDB(pdb_id="PDB1", in_bindingdb_validation_set=False, uniprot=up_entry1)
 
+                                                    #This doest work, use the line above first
+# pdb_entry_1 = db_models.PDB(pdb_id="PDB1", in_bindingdb_validation_set=False, uniprot_id=up_entry1.id)
 
+                            #This format of relationship works for acc since acc is defined for each object
 # pdb_entry_1 = db_models.PDB(pdb_id="PDB1", in_bindingdb_validation_set=True, uniprot_acc=up_entry1.acc)
 # pdb_entry_2 = db_models.PDB(pdb_id="PDB2", in_bindingdb_validation_set=False, uniprot_acc=up_entry1.acc)
+
+db.session.add(pdb_entry_1)
 
 # db.session.add(up_entry2)
 # db.session.add(pdb_entry_2)
